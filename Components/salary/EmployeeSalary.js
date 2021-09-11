ab618c4335c28437791f798440018b46e169fda0
Yuvaraj <yuvarajuv49@gmail.com>
2021-09-11 19:51:30 +0530

    integrated salaryslip

--- Components/salary/EmployeeSalary.js
+++ Components/salary/EmployeeSalary.js
@@ -1,5 +1,5 @@
-import {useNavigation} from '@react-navigation/native';
-import React, {useEffect, useState} from 'react';
+import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 import {
   Image,
   FlatList,
@@ -21,13 +21,14 @@ import {
   deleteSalary,
   getSalaryInit,
 } from '../../services/SalaryData';
-import {getEmployeeById} from '../../services/EmployeeData';
+import { getEmployeeById } from '../../services/EmployeeData';
 import SelectDropdown from 'react-native-select-dropdown';
 
-const Item = ({item, onPress, style, onDelete, onEdit, onSelectMonth}) => (
+
+const Item = ({ item, onPress, style, onDelete, onEdit, onSelectMonth, navigatepayslip }) => (
   <TouchableOpacity
     onPress={onPress}
-    style={[styles.item, style, {borderRadius: 10}]}>
+    style={[styles.item, style, { borderRadius: 10 }]}>
     <View
       style={{
         flex: 1,
@@ -36,41 +37,43 @@ const Item = ({item, onPress, style, onDelete, onEdit, onSelectMonth}) => (
         alignItems: 'stretch',
       }}>
       {/* {console.log("ggggg",item)} */}
-      <View style={{flex: 8, height: 50, flexDirection: 'row'}}>
-        <View style={{flex: 7, height: 50}}>
+      <View style={{ flex: 8, height: 50, flexDirection: 'row' }}>
+        <View style={{ flex: 7, height: 50 }}>
           <Text style={styles.title}>
             {getEmployeeById(item.employeeId).name}
           </Text>
         </View>
         {/* <View style={{flex: 1, height: 40, alignContent: 'space-around'}}> */}
-          <Button
-            onPress={() => {
-              console.log('edit', item.id);
-              onEdit(item.id);
-            }}
-            color="purple"
-            title="Edit"
-          />
+        <Button
+          onPress={() => {
+            console.log('edit', item.id);
+            onEdit(item.id);
+          }}
+          color="purple"
+          title="Edit"
+        />
         {/* </View> */}
 
         <Button
           onPress={() => {
             console.log('select month', item.employeeId);
-            onSelectMonth(item.employeeId);
+            //onSelectMonth(item.employeeId);
+
+            navigatepayslip()
           }}
           color="purple"
           title="Pay-Slip"
         />
 
         {/* <View style={{flex: 1, height: 50}}> */}
-          <Button
-            onPress={() => {
-              console.log('delete', item.id);
-              onDelete(item.id);
-            }}
-            color="purple"
-            title="X"
-          />
+        <Button
+          onPress={() => {
+            console.log('delete', item.id);
+            onDelete(item.id);
+          }}
+          color="purple"
+          title="X"
+        />
         {/* </View> */}
       </View>
       <View
@@ -89,11 +92,11 @@ const Item = ({item, onPress, style, onDelete, onEdit, onSelectMonth}) => (
         <Text style={styles.TotalSalary}>
           Total-Salary   :{' '}
           {parseInt(item.basic) +
-            parseInt(item.lta )+
-            parseInt(item.hra)+
+            parseInt(item.lta) +
+            parseInt(item.hra) +
             parseInt(item.variable) +
             parseInt(item.bonus) +
-            parseInt(item.TDS)+
+            parseInt(item.TDS) +
             parseInt(item.tax)}
         </Text>
         <Text style={styles.address}>
@@ -112,20 +115,21 @@ const EmployeeSalary = props => {
   const [count, doRender] = useState(0);
   //   const [salary,setSalary]=useState([]);
   //   const [salaryInit,setSalaryInit] = useState([]);
-  const [employeeId, setEmployeeId]=useState('');
+  const [employeeId, setEmployeeId] = useState('');
   const [salaryList, setSalaryList] = useState([]);
   const [monthSalary, setMonthSalary] = useState([]);
   const [monthList, setMonthList] = useState([]);
   const navigation = useNavigation();
   const [fetch, setFetch] = useState(true);
+  const nav = useNavigation();
 
   const doFetch = () => {
     const fetchedEmpId = props.route.params;
-    console.log("ooooooooo",fetchedEmpId);
+    console.log("ooooooooo", fetchedEmpId);
     if (fetchedEmpId.employeeId) {
-        setEmployeeId(fetchedEmpId.employeeId);
+      setEmployeeId(fetchedEmpId.employeeId);
     }
-    console.log("ooooooooo",employeeId);
+    console.log("ooooooooo", employeeId);
 
     let empSalaryList = getSalaryListForEmp(employeeId ? employeeId : fetchedEmpId.employeeId);
     console.log('in salaryList :', empSalaryList);
@@ -145,9 +149,10 @@ const EmployeeSalary = props => {
   //   useEffect(async ()=>{
   //     doFetch();
   //   },[])
-  const renderItem = ({item}) => {
+  const renderItem = ({ item }) => {
     console.log('in render in single salary //////////////////');
     const backgroundColor = item.id === selectedId ? 'lightgrey' : 'white';
+
     return (
       <Item
         item={item}
@@ -156,27 +161,32 @@ const EmployeeSalary = props => {
         onDelete={id => {
           // console.log("gggggggggg",id,)
           // customers = customers.filter((e)=>{return e.id!==id})
-          deleteSalary({id});
+          deleteSalary({ id });
           doFetch();
           doRender(count + 1);
         }}
         onEdit={id => {
           let empSalary = getSalaryById(id);
           // console.log("ttttt",customer)
-            navigation.navigate("EditSalary",{...empSalary,doFetch});
+          navigation.navigate("EditSalary", { ...empSalary, doFetch });
         }}
         onSelectMonth={empId => {
           let empSalaryList = getSalaryListForEmp(empId);
           console.log('salary list:', empSalaryList);
         }}
-        style={{backgroundColor}}
+        navigatepayslip={() => {
+
+          nav.navigate('SalarySlip', { id: item.id })
+        }
+        }
+        style={{ backgroundColor }}
       />
     );
   };
 
   return (
     <SafeAreaView style={styles.container}>
-      <View style={{backgroundColor: 'rgb(174, 135, 196)'}}>
+      <View style={{ backgroundColor: 'rgb(174, 135, 196)' }}>
         <View
           style={{
             display: 'flex',
@@ -184,47 +194,48 @@ const EmployeeSalary = props => {
             borderBottomRightRadius: 10,
             backgroundColor: '#dddddc',
           }}></View>
-        <View style={{height: 50, width: '50%',display:"flex",flexDirection:"row",marginVertical:10,justifyContent:"space-between"}}>
-          <View style={{width:"90%",marginLeft:10}} >
-          <SelectDropdown
-            data={monthList}
-            onSelect={(selectedItem, index) => {
-              console.log(selectedItem, index, salaryList[0].monthYear);
-              setMonthSalary(
-                salaryList.filter(item => {
-                  console.log(';;;;;;;;', item.monthYear === selectedItem);
-                  return item.monthYear === selectedItem;
-                }),
-              );
-              console.log(monthSalary);
+        <View style={{ height: 50, width: '50%', display: "flex", flexDirection: "row", marginVertical: 10, justifyContent: "space-between" }}>
+          <View style={{ width: "90%", marginLeft: 10 }} >
+            <SelectDropdown
+              data={monthList}
+              onSelect={(selectedItem, index) => {
+                console.log(selectedItem, index, salaryList[0].monthYear);
+                setMonthSalary(
+                  salaryList.filter(item => {
+                    console.log(';;;;;;;;', item.monthYear === selectedItem);
+                    return item.monthYear === selectedItem;
+                  }),
+                );
+                console.log(monthSalary);
 
-              doRender(count + 1);
-            }}
-            buttonStyle={{
-              backgroundColor: 'lightgrey',
-              width: '90%',
-              alignSelf: 'center',
-              borderRadius: 10,
-            }}
-            buttonTextStyle={{color:"purple"}}
-            dropdownStyle={{backgroundColor: 'white', borderRadius: 10}}
-            defaultButtonText={'Select Month'}
-            buttonTextAfterSelection={(selectedItem, index) => {
-              return selectedItem;
-            }}
-            rowTextForSelection={(item, index) => {
-              return item;
-            }}
-          />
+                doRender(count + 1);
+              }}
+              buttonStyle={{
+                backgroundColor: 'lightgrey',
+                width: '90%',
+                alignSelf: 'center',
+                borderRadius: 10,
+              }}
+              buttonTextStyle={{ color: "purple" }}
+              dropdownStyle={{ backgroundColor: 'white', borderRadius: 10 }}
+              defaultButtonText={'Select Month'}
+              buttonTextAfterSelection={(selectedItem, index) => {
+                return selectedItem;
+              }}
+              rowTextForSelection={(item, index) => {
+                return item;
+              }}
+            />
           </View>
-          <View style={{backgroundColor:"skyblue",width:"90%",justifyContent:"center",marginLeft:10,borderRadius:10}} >
-                <Button color="purple" onPress={()=>{
-                    console.log("employeeId......... ",employeeId);
-                    navigation.navigate('AddSalary',{employeeId:employeeId,doFetch,setEmployeeId})}} title="Add Salary" />
-            </View>
+          <View style={{ backgroundColor: "skyblue", width: "90%", justifyContent: "center", marginLeft: 10, borderRadius: 10 }} >
+            <Button color="purple" onPress={() => {
+              console.log("employeeId......... ", employeeId);
+              navigation.navigate('AddSalary', { employeeId: employeeId, doFetch, setEmployeeId })
+            }} title="Add Salary" />
+          </View>
 
         </View>
-        <View style={{height: '89%', overflowY: 'scroll'}}>
+        <View style={{ height: '89%', overflowY: 'scroll' }}>
           <FlatList
             data={monthSalary.length ? monthSalary : salaryList}
             renderItem={renderItem}
@@ -258,7 +269,7 @@ const styles = StyleSheet.create({
   },
   TotalSalary: {
     fontSize: 18,
-    color:"purple"
+    color: "purple"
   },
   address: {
     fontSize: 16,
