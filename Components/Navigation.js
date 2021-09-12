e7faf91a94c8a7d011925a322c7bf2f9ca95da82
Izhan Rafiq <31858677+izhanrafiq@users.noreply.github.com>
2021-09-12 15:13:15 +0530

    updated navigation.js

--- Components/Navigation.js
+++ Components/Navigation.js
@@ -1,29 +1,40 @@
 import * as React from 'react';
 import { BottomNavigation, Text } from 'react-native-paper';
+import EmployeeApp from './EmployeeList';
+import Salary from './salary/Salary';
+import { useNavigation } from '@react-navigation/core';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
 
-const EmployeeRoute = () => <Text>Employee</Text>;
 
-const SalarysRoute = () => <Text>Salary</Text>;
+const Nav = (props) => {
+    React.useEffect(()=>{
+        },[]);
 
-const Navigation = () => {
+    
   const [index, setIndex] = React.useState(0);
+  const [render,setRender]=React.useState(0);
   const [routes] = React.useState([
-    { key: 'employee', title: 'Employee', icon: 'album' },
-    { key: 'salary', title: 'Salary', icon: 'history' },
+    { key: 'albums', title: 'Employee', icon:require('../public/images/emp.png')},
+      { key: 'salary', title: 'Salary',     icon:require('../public/images/money.png')},
   ]);
 
-  const renderScene = BottomNavigation.SceneMap({
-    employee: EmployeeRoute,
-    salarys: SalarysRoute,
-  });
+  renderScene = ({ route, jumpTo }) => {
+  switch (route.key) {
+    case 'salary':
+      return <Salary jumpTo={jumpTo} />;
+    case 'albums':
+      return <EmployeeApp jumpTo={jumpTo} />;
+  }
+}
 
   return (
     <BottomNavigation
       navigationState={{ index, routes }}
       onIndexChange={setIndex}
       renderScene={renderScene}
+      shifting={true}
     />
   );
 };
 
-export default Navigation;
+export default Nav;
